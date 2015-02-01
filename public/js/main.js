83f0633138b9a0f0d09ee25dda953744273828bc
Abhinay Omkar <abhiomkar@gmail.com>
2015-02-01 19:48:07 +0530

    added handlebar support, added firebase update method, other changes to app

--- public/js/main.js
+++ public/js/main.js
@@ -1,79 +1,162 @@
 $(function() {
-	// do not pick up pieces if the game is over
-	// only pick up pieces for the side to move
-	var onDragStart = function(source, piece, position, orientation) {
-	  if (game.game_over() === true ||
-	      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-	      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-	    return false;
-	  }
-	};
 
-	var onDrop = function(source, target) {
-	  // see if the move is legal
-	  var move = game.move({
-	    from: source,
-	    to: target,
-	    promotion: 'q' // NOTE: always promote to a queen for example simplicity
-	  });
+    var App = function($el) {
+        this.gameId;
+        this.init();
+        this.load();
+    };
 
-	  // illegal move
-	  if (move === null) return 'snapback';
+    App.fn = App.prototype;
 
-	  updateStatus();
-	};
+    App.fn.init = function() {
 
-	// update the board position after the piece snap 
-	// for castling, en passant, pawn promotion
-	var onSnapEnd = function() {
-	  board.position(game.fen());
-	};
+        console.log("Init");
 
-	var updateStatus = function() {
-	  var status = '';
+        this.regex = {
+            validPlay: /play\/(\w+).*/g
+        };
+    };
 
-	  var moveColor = 'White';
-	  if (game.turn() === 'b') {
-	    moveColor = 'Black';
-	  }
+    App.fn.load = function() {
+        var self = this;
 
-	  // checkmate?
-	  if (game.in_checkmate() === true) {
-	    status = 'Game over, ' + moveColor + ' is in checkmate.';
-	  }
+        this.gameId = this.getGameIdFromUrl();
 
-	  // draw?
-	  else if (game.in_draw() === true) {
-	    status = 'Game over, drawn position';
-	  }
+        if (this.gameId) {
+            this.getGameData((function(data) {
+                this.gameConfig = {
+                    pieceTheme: '/libs/chessboardjs/img/chesspieces/wikipedia/{piece}.png',
+                    draggable: true,
+                    orientation: 'white',
+                    onDragStart: this.Events().onDragStart,
+                    onDrop: this.Events().onDrop,
+                    onSnapEnd: this.Events().onSnapEnd,
+                    position: data.position
+                };
 
-	  // game still on
-	  else {
-	    status = moveColor + ' to move';
+                this.game = new Chess();
 
-	    // check?
-	    if (game.in_check() === true) {
-	      status += ', ' + moveColor + ' is in check';
-	    }
-	  }
+                this.board = new ChessBoard('board', this.gameConfig);
+                this.board.start();
 
-	};
+                window.board = this.board;
+            }).bind(this));
+        }
+    };
 
-	// position: 'rnb1kbnr/pppp1ppp/8/4p3/6Pq/5P2/PPPPP2P/RNBQKBNR',
+    App.fn.getGameData = function(callback) {
+            $.ajax({
+                url: "/game/" + this.gameId,
+                method: "get",
+                dataType: "json"
+            })
+            .done(function(data) {
+                callback(data);
+            })
+            .error(function() {
+                console.log("Error loading game data.");
+            });
+    };
 
-	var cfg = {
-		pieceTheme: 'libs/chessboardjs/img/chesspieces/wikipedia/{piece}.png',
-		draggable: true,
-		orientation: 'white',
-		onDragStart: onDragStart,
-		onDrop: onDrop,
-	    onSnapEnd: onSnapEnd
-	};
+    App.fn.getGameIdFromUrl = function() {
+        var match = this.regex.validPlay.exec(window.location.pathname),
+            gameId;
 
-	var game = new Chess();
+        if (match) {
+            gameId = match[1];
+            return gameId;
+        }
+        else {
+            return null;
+        }
+    };
 
-	var board = new ChessBoard('board', cfg);
-	board.start();
+    App.fn.Events = function() {
+        var self = this;
 
-	window.board = board;
+        return {
+            // do not pick up pieces if the game is over
+            // only pick up pieces for the side to move
+            onDragStart: function(source, piece, position, orientation) {
+              if (self.game.game_over() === true ||
+                  (self.game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+                  (self.game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+                return false;
+              }
+            },
+
+            onDrop: function(source, target) {
+              // see if the move is legal
+              var move = self.game.move({
+                from: source,
+                to: target,
+                promotion: 'q' // NOTE: always promote to a queen for example simplicity
+              });
+
+              // illegal move
+              if (move === null) return 'snapback';
+
+              self.updateStatus();
+            },
+
+            // update the board position after the piece snap 
+            // for castling, en passant, pawn promotion
+            onSnapEnd: function() {
+              board.position(self.game.fen());
+            }
+        }
+    };
+
+    App.fn.updateStatus = function() {
+      var status = '',
+          fen = this.game.fen().split(' ')[0];
+
+      var moveColor = 'White';
+      if (this.game.turn() === 'b') {
+        moveColor = 'Black';
+      }
+
+      // checkmate?
+      if (this.game.in_checkmate() === true) {
+        status = 'Game over, ' + moveColor + ' is in checkmate.';
+      }
+
+      // draw?
+      else if (this.game.in_draw() === true) {
+        status = 'Game over, drawn position';
+      }
+
+      // game still on
+      else {
+        status = moveColor + ' to move';
+
+        // check?
+        if (this.game.in_check() === true) {
+          status += ', ' + moveColor + ' is in check';
+        }
+      }
+
+      console.log("gameId: ", this.gameId);
+      console.log("fen: ", fen);
+
+      $.ajax({
+        url: "/game/" + this.gameId,
+        type: "PUT",
+        dataType: "json",
+        data: { position: fen }
+      })
+      .done(function(response) {
+         console.log('Updated.');
+      })
+      .error(function(error) {
+         console.log('Error: ', error);
+      });
+
+    };
+
+    // position: 'rnb1kbnr/pppp1ppp/8/4p3/6Pq/5P2/PPPPP2P/RNBQKBNR',
+
+    var app = new App();
+
+
 });
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
-router.get('/game/:id/white', function(req, res) {
+router.get('/play/:id([a-zA-Z]+)/:color(white|black)/?', function(req, res, next) {
+  console.log('play!');
+  res.render('index', {title: 'hello'});
+});
+
+router.get('/game/:id', function(req, res) {
 	var gameId = req.params.id, 
 		firebase = new Firebase('https://chessboard-io.firebaseio.com/' + gameId);
 
@@ -26,6 +31,26 @@ router.get('/game/:id/white', function(req, res) {
 				res.set('Content-type', 'application/json');
 				res.send(JSON.stringify({ position: "start"}));	
 			});
+		}
+	});
+});
+
+router.put('/game/:id/?', function(req, res) {
+	var gameId = req.params.id,
+		firebase = new Firebase('https://chessboard-io.firebaseio.com/');
+
+	console.log('gameId: ', gameId);
+	firebase.child(gameId);
+	firebase.update(req.body, function(error) {
+		if (error) {
+			console.log('Error in put', error);
+			res.set('Content-type', 'application/json');
+			res.send(JSON.stringify({}));
+		}
+		else {
+			console.log('Put success.');
+			res.set('Content-type', 'application/json');
+			res.send(JSON.stringify({}));
 		}
 	});
 });
